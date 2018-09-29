4fa2ce26dd2643f627bb348ab9e43dc3d8676f30
backf1re <backf1re@users.noreply.github.com>
2018-09-29 20:10:40 +0300

    code corrections
    corrected usage "exec" in bot.js and cloned bot-c.js to implement c++ library

--- bot.js
+++ bot.js
@@ -2,13 +2,12 @@ var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
 var exec = require('child_process').execFile;
-var runq2 =function(){
-             exec('"D:\games\q2pro-server\q2proded.exe"', "+game opentdm", "+exec srv.cfg", function(err, data) {  
-             console.log(err)
-             console.log(data.toString());
-			 console.log(stdout);
-             });  
-
+var runq2 = function(){
+        exec('./D:/games/q2pro-server/q2proded.exe', function(err, data) { 
+			console.log(err)
+			console.log(data.toString())
+		})
+}
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -21,29 +20,30 @@ var bot = new Discord.Client({
    autorun: true
 });
 bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info('Connected')
+    logger.info('Logged in as: ' + bot.username + ' - (' + bot.id + ')')
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
+    bot.sendMessage({
+                    to: channelID,
+                    message: 'Q2bot at your service'
+       		});
+	if (message.substring(0, 1) == '!') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-       
         args = args.splice(1);
         switch(cmd) {
             // !startsrv
             case 'startsrv':
-		bot.sendMessage({
+			bot.sendMessage({
                     to: channelID,
                     message: 'Starting Q2server'
        		});
-		runq2();
-	};
-            break;
+			runq2();
+		    break;
             // Just add any case commands if you want to..
-         }
-     }
+		}
+	}
 });
